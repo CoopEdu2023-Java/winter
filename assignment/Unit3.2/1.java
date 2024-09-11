40e88d563b464b92a82af5e45919ac5c3fe4e879
meow <winter7z@163.com>
2024-09-12 00:12:58 +0800

    Assignment 3.2

---
+++ assignment/Unit3.2/1.java
@@ -0,0 +1,11 @@
+class TreeNode {
+    int value;
+    TreeNode brother;
+    TreeNode child;
+
+    public TreeNode(int value) {
+        this.value = value;
+        this.brother = null;
+        this.child = null;
+    }
+}
