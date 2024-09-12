a031e74bcf766bd8951f9066a50a4c048c91e5fb
meow <winter7z@163.com>
2024-09-12 14:49:16 +0800

    upload .gitignore

---
+++ assignment/Unit1.1/2.java
@@ -0,0 +1,9 @@
+class Solution {
+    public void isEqual(int a, int b) {
+        if (a == b) {
+            System.out.println("a and b are equal");
+        } else {
+            System.out.println("a and b are not equal");
+        }
+    }
+}
---
+++ assignment/Unit1.1/3.java
@@ -0,0 +1,7 @@
+class Solution {
+    public void main(int num) {
+        for (int i = 1; i <= pow(num, 2); i++) {
+
+        }
+    }
+}
