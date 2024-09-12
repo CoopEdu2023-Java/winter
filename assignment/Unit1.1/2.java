fdfe36807e0fb1b87d55ef746f17fcc94a2ea888
meow <winter7z@163.com>
2024-09-12 14:57:10 +0800

    Update

--- assignment/Unit1.1/2.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public void isEqual(int a, int b) {
-        if (a == b) {
-            System.out.println("a and b are equal");
-        } else {
-            System.out.println("a and b are not equal");
-        }
-    }
-}
--- assignment/Unit1.1/3.java
+++
@@ -1,7 +0,0 @@
-class Solution {
-    public void main(int num) {
-        for (int i = 1; i <= pow(num, 2); i++) {
-
-        }
-    }
-}
