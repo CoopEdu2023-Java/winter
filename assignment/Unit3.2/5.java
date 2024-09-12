1e6db60e514945444c3afe8357d6addb480b29da
meow <winter7z@163.com>
2024-09-12 14:37:44 +0800

    Change the structure of folders

--- assignment/Unit3.2/5.java
+++ assignment/Unit3.2/5.java
@@ -1,5 +1,27 @@
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<Integer>();
+            int size = queue.size();
+            for (int i = 1; i <= size; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(level);
+        }
 
+        return result;
     }
 }
