a2bb7b4568e0c41e406db588ccbdfaea59882365
meow <winter7z@163.com>
2024-11-04 15:18:25 +0800

    assignment 4.2

---
+++ assignment/Unit4.2/FileDemo.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.FileFilter;
+
+public class FileDemo {
+    public static void main(String[] args) {
+        File dir = new File("./");
+        if (dir.isDirectory()) {
+            listJavaFiles(dir);
+        }
+    }
+
+    private static void listJavaFiles(File dir) {
+        FileFilter filter = (File file) -> file.isFile() && file.getName().endsWith(".java");
+        File[] files = dir.listFiles();
+
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    listJavaFiles(file);
+                } else if (filter.accept(file)) {
+                    System.out.println(file.getAbsolutePath());
+                }
+            }
+        }
+    }
+}
